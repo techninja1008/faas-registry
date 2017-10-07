9d631a06aa27dd29d0762a1db927c2a6901d30fc
Danny Wensley <dannywensley7@gmail.com>
2017-10-07 16:40:41 +0000

    Better node cache invalidation, because it really wasn't working before.

--- gateway.js
+++ gateway.js
@@ -8,6 +8,55 @@ const targz = require('targz');
 const FunctionParser = require('faaslang').FunctionParser;
 const path = require('path');
 
+// === CODE STOLEN SHAMELESSLY FROM https://stackoverflow.com/a/14801711/2555702 ===
+
+/**
+ * Removes a module from the cache
+ */
+function purgeCache(moduleName) {
+    // Traverse the cache looking for the files
+    // loaded by the specified module name
+    searchCache(moduleName, function (mod) {
+        delete require.cache[mod.id];
+    });
+
+    // Remove cached paths to the module.
+    // Thanks to @bentael for pointing this out.
+    Object.keys(module.constructor._pathCache).forEach(function(cacheKey) {
+        if (cacheKey.indexOf(moduleName)>0) {
+            delete module.constructor._pathCache[cacheKey];
+        }
+    });
+};
+
+/**
+ * Traverses the cache to search for all the cached
+ * files of the specified module name
+ */
+function searchCache(moduleName, callback) {
+    // Resolve the module identified by the specified name
+    var mod = require.resolve(moduleName);
+
+    // Check if the module has been resolved and found within
+    // the cache
+    if (mod && ((mod = require.cache[mod]) !== undefined)) {
+        // Recursively go over the results
+        (function traverse(mod) {
+            // Go over each of the module's children and
+            // traverse them
+            mod.children.forEach(function (child) {
+                traverse(child);
+            });
+
+            // Call the specified callback providing the
+            // found cached module
+            callback(mod);
+        }(mod));
+    }
+};
+
+// === END SO STEAL ===
+
 class MultiTennantGateway extends Gateway {
 
   constructor(cfg) {
@@ -158,7 +207,7 @@ class MultiTennantGateway extends Gateway {
     let fn;
     try {
       let rpath = require.resolve(path.join(definition.mtroot, definition.pathname));
-      delete require[rpath];
+      purgeCache(path.join(definition.mtroot, definition.pathname)) // Better than before!
       fn = require(rpath);
     } catch (e) {
       e.fatal = true;
